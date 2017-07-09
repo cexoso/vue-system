e280f9c5b111b683b9190be4399f74eb1969a2a6
You <you85630@foxmail.com>
2017-07-09 15:02:32 +0800

    树形控件

--- src/components/pages/tree.vue
+++ src/components/pages/tree.vue
@@ -1,32 +1,88 @@
 <template>
-    <div class="treegrid">
-      <Tree :data="baseData" show-checkbox></Tree>
-    </div>
+  <div class="tree">
+    <tree :tree="list"></tree>
+  </div>
 </template>
+
 <script>
-    export default {
-      data () {
-        return {
-          baseData: [{
-            expand: true,
-            title: '父类 1',
-            children: [{
-              title: '父类 1-0',
-              expand: true,
-              children: [{
-                title: '子类'
-              }, {
-                title: '子类'
-              }]
+import tree from 'components/common/treegrid'
+export default {
+  components: {
+    tree
+  },
+  name: 'treelist',
+  data () {
+    return {
+      list: [
+        {
+          id: '1',
+          name: '1',
+          type: 'tree',
+          expanded: true,
+          indent: '1',
+          children: [
+            {
+              id: '1-1',
+              name: '1-1',
+              type: 'tree',
+              expanded: true,
+              indent: '2',
+              children: [
+                {
+                  id: '1-1-1',
+                  name: '1-1-1',
+                  type: 'tree',
+                  expanded: true,
+                  indent: '3'
+                }, {
+                  id: '1-1-2',
+                  name: '1-1-2',
+                  type: 'tree',
+                  expanded: true,
+                  indent: '3',
+                  children: [
+                    {
+                      id: '1-1-1-1',
+                      name: '1-1-1-1',
+                      type: 'tree',
+                      expanded: true,
+                      indent: '4'
+                    }
+                  ]
+                }, {
+                  id: '1-1-3',
+                  name: '1-1-3',
+                  type: 'tree',
+                  expanded: true,
+                  indent: '3'
+                }
+              ]
             }, {
-              title: '父类 1-1',
-              expand: true,
-              children: [{
-                title: '子类'
-              }]
-            }]
-          }]
+              id: '1-2',
+              name: '1-2',
+              type: 'tree',
+              expanded: true,
+              indent: '2'
+            }
+          ]
+        }, {
+          id: '2',
+          name: '2',
+          type: 'tree',
+          expanded: true,
+          indent: '1'
         }
-      }
+      ]
     }
+  }
+}
 </script>
+
+<style scoped lang="scss">
+.tree {
+  width: 600px;
+  margin: 10px auto;
+  border: 1px solid #ccc;
+  border-bottom: none;
+}
+</style>
