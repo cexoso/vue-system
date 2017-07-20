b7b5a87ba0a34e98348cfc8dcb9ef0af19a0f96c
You <you85630@foxmail.com>
2017-07-20 19:39:37 +0800

    删除log

--- src/vuex/modules/leftnav.js
+++ src/vuex/modules/leftnav.js
@@ -39,7 +39,6 @@ export default {
     leftMenu ({commit}, res) {
       api.get('/leftMenu')
         .then(function (res) {
-          console.log(res)
           commit('leftMenu', res.data)
         })
     }
